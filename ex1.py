8462cb37bffaf4da656e05a86b2fdc37149ba976
Yan <yanbaldissera1@gmail.com>
2023-09-06 14:50:17 -0300

    atualizacao1

--- ex1.py
+++ ex1.py
@@ -1,13 +1,69 @@
-def tabuleiro():
-    cont = 0
+def criarTabuleiro():
+    """ Vai retornar o tabuleiro """
     tabuleiro = [[" " for _ in range(4)] for _ in range(4)]
+    return tabuleiro
+    
+        
+def tabuleiro(tabuleiro):
+    "vai imprimir o tabuleiro no formato"
     for linha in tabuleiro:
-        print(f"{cont}".join(linha))
-        print(f"{cont}")
         print("  |  ".join(linha))
         print("-" * 20)
-        cont += 1
-        
-
-tabuleiro()
     
+
+def verificarVitoria(tabuleiro, jogador):
+    """ Vai retornar se já houve algum ganhador ou não """
+    for linhas in tabuleiro:
+        if all(posicao == jogador for posicao in linhas):
+            return True
+    
+    for colunas in range(4):
+        colunaAtual = [tabuleiro[linhas][colunas] for linhas in range(4)]
+        
+        if all(posicao == jogador for posicao in colunaAtual):
+            return True
+        
+    
+    diagonalPrimaria = [tabuleiro[i][i] for i in range(4)]
+    if all(posicao == jogador for posicao in diagonalPrimaria):
+        return True
+    
+    diagonalSecundaria = [tabuleiro[i][3 - i] for i in range(4)]
+    if all(posicao == jogador for posicao in diagonalSecundaria):
+        return True
+    
+    
+    return False
+
+
+
+def jogoDaVelha():
+    """ Vai retornar o jogo """
+    
+    jogo = criarTabuleiro()
+    
+    jogadorAtual = "X"
+    
+    while True:
+        tabuleiro(jogo)
+        
+        print(f"Jogador {jogadorAtual}, é a sua vez.")
+        linha = int(input("Digite o número da linha (0-3)"))    
+        coluna = int(input("Digite o número da coluna (0-3)"))
+        
+        if 0 <= linha < 4 and 0 <= coluna < 4 and jogo[linha][coluna] == " ":
+            jogo[linha][coluna] = jogadorAtual
+            
+            if verificarVitoria(jogo, jogadorAtual):
+                tabuleiro(jogo)
+                print(f"Jogador {jogadorAtual} venceu!!!")
+                break
+            else:
+               jogadorAtual = "O" if jogadorAtual == "X" else "X"
+                
+        else:
+            print("Jogada inválida. Tente novamente")
+            
+
+jogoDaVelha()
+            
